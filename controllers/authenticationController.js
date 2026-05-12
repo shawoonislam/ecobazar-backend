47cddab9536a9558761adb6eb79464a446b4e0fa
Sajib Saha <gta3.cse@diu.edu.bd>
2026-05-12 19:35:21 +0600

    some changes

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,149 +1,210 @@
-const { mailVerification, resetPasswordMail } = require("../utils/email")
-const User = require('../models/userModel')
+const { mailVerification, resetPasswordMail } = require("../utils/email");
+const User = require("../models/userModel");
 
-const { emptyFieldValidation } = require("../utils/validation")
-const tokenGenerator = require("../utils/tokenGenerator")
-const existingData = require("../utils/exixtingData")
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const { emptyFieldValidation } = require("../utils/validation");
+const tokenGenerator = require("../utils/tokenGenerator");
+const existingData = require("../utils/exixtingData");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 let registrationController = async (req, res) => {
-    const { email, password, confirmPassword, terms } = req.body
+  const { email, password, confirmPassword, terms } = req.body;
 
-    let users = await existingData(res, { email: email })
-    if (users) {
-        return res.send({ message: "User exist" })
-    }
+  let users = await existingData(res, { email: email });
+  if (users) {
+    return res.send({
+      success: false,
+      message: "User exist",
+    });
+  }
 
-    if (!terms) {
-        return res.send({ message: "Please Accept Our Terms and Condition" })
-    }
+  if (!terms) {
+    return res.send({
+      success: false,
+      message: "Please Accept Our Terms and Condition",
+    });
+  }
 
+  emptyFieldValidation(res, email, password, confirmPassword);
 
-    emptyFieldValidation(res, email, password, confirmPassword)
+  if (password !== confirmPassword) {
+    return res.send({
+      success: false,
+      message: "password no matched",
+    });
+  }
 
-    if (password !== confirmPassword) {
-        return res.send({ message: "password no matched" })
-    }
+  const hash = bcrypt.hashSync(password, 10);
 
-    const hash = bcrypt.hashSync(password, 10);
+  let user = new User({
+    email: email,
+    password: hash,
+    terms: terms,
+  });
 
-    let user = new User({
-        email: email,
-        password: hash,
-        terms: terms
-    })
+  await user.save();
 
-    user.save()
+  let token = tokenGenerator(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    "1d",
+  );
 
-    let token = tokenGenerator({
-        id: user._id,
-        email: user.email
-    }, process.env.ACCESS_TOKEN_SECRET, "1d")
+  mailVerification(token, email);
 
-    mailVerification(token, email)
-
-    res.send({ message: "Registration Successfull" })
-
-}
+  res.send({
+    success: true,
+    message: "Registration Successfull, Please check your email for verification",
+  });
+};
 
 let loginController = async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    let users = await User.findOne({ email: email })
-    if (!users) {
-        return res.send({ message: "User not found" })
-    }
-    emptyFieldValidation(res, email, password)
+  let users = await User.findOne({ email: email });
+  if (!users) {
+    return res.send({ 
+        success: false,
+        message: "User not found" 
+    });
+  }
+  emptyFieldValidation(res, email, password);
 
-    let pass = bcrypt.compareSync(password, users.password);
+  let pass = bcrypt.compareSync(password, users.password);
 
-    if (!pass) {
-        return res.send({ message: "Invalid Credential" })
-    }
+  if (!pass) {
+    return res.send({
+      success: false,
+      message: "Invalid Credential",
+    });
+  }
 
-    res.send({
-        message: "Login Successfull"
-    })
-
-}
+  res.send({
+    success: true,
+    message: "Login Successfull",
+  });
+};
 
 let forgotPasswordController = async (req, res) => {
-    let { email } = req.body
-    emptyFieldValidation(res, email)
-    let users = await User.findOne({ email: email })
-    if (!users) {
-        return res.send({ message: "User not found" })
-    }
+  let { email } = req.body;
+  emptyFieldValidation(res, email);
+  let users = await User.findOne({ email: email });
+  if (!users) {
+    return res.send({
+      success: false,
+      message: "User not found",
+    });
+  }
 
-    let token = tokenGenerator({
-        id: users._id,
-        email: users.email
-    }, process.env.ACCESS_TOKEN_SECRET, "1d")
+  let token = tokenGenerator(
+    {
+      id: users._id,
+      email: users.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    "1d",
+  );
 
-    resetPasswordMail(token, email)
+  resetPasswordMail(token, email);
 
-    res.send({ message: "PLease check your email" })
-}
+  res.send({
+    success: true,
+    message: "Please check your email",
+  });
+};
 
 let resetPasswordController = (req, res) => {
-    let { newPassword, confirmPassword } = req.body
-    let { token } = req.params
+  let { newPassword, confirmPassword } = req.body;
+  let { token } = req.params;
 
-    if (newPassword !== confirmPassword) {
-        return res.send({ message: "Confirm password not matched" })
-    }
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async function (err, decoded) {
-        if (err) {
-            res.send({ message: "Unauthorized" })
-        } else {
-            const hash = bcrypt.hashSync(newPassword, 10);
-            console.log(decoded)
-            const updateData = await User.findByIdAndUpdate({ _id: decoded.id }, { password: hash }, { new: true })
-            res.send({ message: "Password Updated", updateData })
-        }
+  if (newPassword !== confirmPassword) {
+    return res.send({
+      success: false,
+      message: "Confirm password not matched",
     });
-}
+  }
+
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    async function (err, decoded) {
+      if (err) {
+        res.send({ message: "Unauthorized" });
+      } else {
+        const hash = bcrypt.hashSync(newPassword, 10);
+        console.log(decoded);
+        const updateData = await User.findByIdAndUpdate(
+          { _id: decoded.id },
+          { password: hash },
+          { new: true },
+        );
+        res.send({
+          success: true,
+          message: "Password Updated",
+          updateData,
+        });
+      }
+    },
+  );
+};
 
 let resendVerificationEmailController = async (req, res) => {
-    let { email } = req.body
+  let { email } = req.body;
 
-    let user = await User.findOne({ email: email })
+  let user = await User.findOne({ email: email });
 
-    let token = tokenGenerator({
-        id: user._id,
-        email: user.email
-    }, process.env.ACCESS_TOKEN_SECRET, "1d")
+  let token = tokenGenerator(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    "1d",
+  );
 
-    mailVerification(token, email)
+  mailVerification(token, email);
 
-    res.send({ message: "Check your email for verification" })
-}
-
+  res.send({
+    success: true,
+    message: "Check your email for verification",
+  });
+};
 
 let verifyEmailController = async (req, res) => {
-    const { token } = req.params
+  const { token } = req.params;
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async function (err, decoded) {
-        if (err) {
-            res.send({ message: "Unauthorized" })
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    async function (err, decoded) {
+      if (err) {
+        res.send({ message: "Unauthorized" });
+      } else {
+        const userId = decoded.id;
+        let findUser = await User.findById(userId);
+        if (findUser.isVerified) {
+          return res.send({ message: "User already verified" });
         } else {
-            const userId = decoded.id
-            let findUser = await User.findById(userId)
-            if (findUser.isVerified) {
-                return res.send({ message: "User already verified" })
-            } else {
-                findUser.isVerified = true
-                findUser.save()
-                res.send({ message: "Email verified successfully" })
-            }
-
+          findUser.isVerified = true;
+          findUser.save();
+          res.send({
+            success: true,
+            message: "Email verified successfully",
+          });
         }
-    })
+      }
+    },
+  );
+};
 
-}
-
-
-
-module.exports = { registrationController, loginController, forgotPasswordController, resetPasswordController, resendVerificationEmailController, verifyEmailController }
+module.exports = {
+  registrationController,
+  loginController,
+  forgotPasswordController,
+  resetPasswordController,
+  resendVerificationEmailController,
+  verifyEmailController,
+};
